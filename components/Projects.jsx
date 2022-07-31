1bf0abae6f2e44b0b57f6ea4684c881c3c6e9c41
Daniela Diaz Graziani <danieladiazgraziani12@gmail.com>
2022-07-31 03:15:54 -0300

    add path

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -8,7 +8,7 @@ import ProjectsItem from "./ProjectsItem";
 
 export default function Projects() {
   return (
-    <div className="w-full">
+    <div id="projects" className="w-full">
       <div className="max-w-[1240px] mx-auto px-2 py-16">
         <h2 className="uppercase tracking-widest mb-12">Projects</h2>
         <div className="grid lg:grid-cols-1 md:grid-cols-2 gap-8">
@@ -16,7 +16,6 @@ export default function Projects() {
        <ProjectsItem title='Recipes Ideas' backgroundImg={recipesIdeas} projectUrl='/' projectRepo='https://github.com/DanielaGraziani/PI-FOODS'/>
        <ProjectsItem title='Free Style Store' backgroundImg={freeStyle} projectUrl='/' projectRepo='https://github.com/DanielaGraziani/front-free-style'/>
        <ProjectsItem title='Take a Seat' backgroundImg={takeASeat} projectUrl='/' projectRepo='https://github.com/DanielaGraziani/Take-A-Seat'/>
-       
         </div>
       </div>
     </div>
