91eef0f23a626da3790ae2f11276d7f325bf1df1
Daniela Diaz Graziani <danieladiazgraziani12@gmail.com>
2022-07-31 03:13:48 -0300

    add path home

--- components/Main.jsx
+++ components/Main.jsx
@@ -5,7 +5,7 @@ export default function Main() {
   
 
   return (
-    <div className='w-full h-screen text-center'>
+    <div id='home' className='w-full h-screen text-center'>
         <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
             <div >
                 <p className='uppercase tracking-widest text-gray-600'> Fullstack Developer</p>
