6d6fd27079fd37bb1d27fda1d9635e15122c4e9c
Daniela Diaz Graziani <danieladiazgraziani12@gmail.com>
2022-08-02 00:05:31 -0300

    Lottie Home

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,23 +1,34 @@
-import React, {useEffect, useRef} from 'react'
-// import Animation from './Animation'
+import React from "react";
+import Web from './Lottie/Web'
+
 
 export default function Main() {
   
 
   return (
-    <div id='home' className='w-full h-screen text-center'>
-        <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
-            <div >
-                <p className='uppercase tracking-widest text-gray-600'> Fullstack Developer</p>
-                <h1 className='py-4 text-gray-700'>Hi, I'm <span className='text-[#5651e5]'>Daniela</span></h1>
-                <h1 className='py-2 text-gray-700'> A Full Stack Web Developer</h1>
-                <p className='py-4 text-gray-600 max-w-[70%] m-auto'>I'm a full stack developer specializing in building (and occasionally designing) exceptional digital experiences. Currently, im focused on building fronted web applications while learning back-end technologies</p>
-            </div>
-            {/* <div className='lg:max-w-lg lg:w-full md:w-1/2'>
-               <Animation/>
-            </div> */}
+    <div id="home" className="w-full h-screen text-center ">
+      <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center flex-wrap">
+        <div className="mt-20 md:grid md:grid-cols-2">
+         
+          <div className="mt-24 ">
+            <h1 className="py-4 text-gray-700">
+              Hi, I'm <span className="text-[#5651e5]">Daniela</span>
+            </h1>
+            <h1 className="py-2 text-gray-700 "> A Full Stack Web Developer</h1>
+            <p className="py-4 text-gray-600 max-w-[70%] m-auto">
+              I'm a full stack developer specializing in building (and
+              occasionally designing) exceptional digital experiences.
+              Currently, im focused on building fronted web applications while
+              learning back-end technologies
+            </p>
+          </div>
+
+          {/* <div className='lg:max-w-lg lg:w-full md:w-1/2'> */}
+      <Web/>
+            {/* </div> */}
           
         </div>
-     </div>
-  )
+      </div>
+    </div>
+  );
 }
