780a188c191f345b0a26a4beb69a016ff563e82b
Daniela Diaz Graziani <danieladiazgraziani12@gmail.com>
2022-08-11 15:51:32 -0300

    add emailjs

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,28 +1,50 @@
-import React from "react";
+import React, { useRef } from "react";
 import Link from "next/link";
+import emailjs from "@emailjs/browser";
 import { AiOutlineMail, AiOutlineWhatsApp } from "react-icons/ai";
 import { FaLinkedinIn, FaGithub } from "react-icons/fa";
 import { HiOutlineChevronDoubleUp } from "react-icons/hi";
 import ContactLottie from "./Lottie/ContactLottie";
 
 export default function Contact() {
+  const form = useRef();
+
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_8byh6gl",
+        "template_mwxc8a9",
+        form.current,
+        "NhHLMncUSpnSgEaWb"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  }
+
   return (
-    <div id='contact' className="w-full lg:h-screen">
+    <div id="contact" className="w-full lg:h-screen">
       <div className="max-w-[1040px] m-auto px-2 py-16 w-full">
         <h2 className="uppercase tracking-widest mb-12">Contacto</h2>
         <div className="grid lg:grid-cols-5 gap-8">
           {/* left */}
           <div className="col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4">
             <div className="lg:p-4 h-full">
-              
-             
               <div className="text-center">
                 <h2 className="py-2 ">Daniela </h2>
                 <p>Fullstack Developer</p>
                 <p>Contáctame y hablemos.</p>
               </div>
 
-                 <ContactLottie/>
+              <ContactLottie />
               <div>
                 <p className="uppercase pt-2"></p>
                 <div className="flex items-center justify-between py-4">
@@ -48,11 +70,12 @@ export default function Contact() {
           {/* right */}
           <div className="col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4">
             <div className="p-4">
-              <form>
+              <form ref={form} onSubmit={sendEmail}>
                 <div className="grid md:grid-cols-2 gap-4 w-full py-2">
                   <div className="flex flex-col">
                     <label className="uppercase text-sm py-2">Nombre</label>
                     <input
+                      name="name"
                       className="border-2 rounded-lg p-3 border-gray-300"
                       type="text"
                     />
@@ -61,6 +84,7 @@ export default function Contact() {
                   <div className="flex flex-col">
                     <label className="uppercase text-sm py-2">Teléfono</label>
                     <input
+                      name="phone"
                       className="border-2 rounded-lg p-3 border-gray-300"
                       type="text"
                     />
@@ -69,6 +93,7 @@ export default function Contact() {
                 <div className="flex flex-col py-2">
                   <label className="uppercase text-sm py-2">Email</label>
                   <input
+                    name="mail"
                     className="border-2 rounded-lg p-3 border-gray-300"
                     type="email"
                   />
@@ -77,6 +102,7 @@ export default function Contact() {
                 <div className="flex flex-col">
                   <label className="uppercase text-sm py-2">Asunto</label>
                   <input
+                    name="subject"
                     className="border-2 rounded-lg p-3 border-gray-300"
                     type="text"
                   />
@@ -84,8 +110,9 @@ export default function Contact() {
                 <div className="flex flex-col">
                   <label className="uppercase text-sm py-2">Mensaje</label>
                   <textarea
+                    name="message"
                     className="border-2 rounded-lg p-3 border-gray-300"
-                    rows="10"
+                    rows="3"
                   ></textarea>
                 </div>
                 <button className="w-full p-4 text-gray-100 mt-4">
@@ -95,9 +122,9 @@ export default function Contact() {
             </div>
           </div>
         </div>
-        <div className="flex justify-center py-12">
+        <div className="flex justify-center">
           <Link href="/">
-            <div className="mt-8 rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer animate-bounce">
+            <div className="mt-16 rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer animate-bounce">
               <HiOutlineChevronDoubleUp className="text-[#5651e5]" size={30} />
             </div>
           </Link>
