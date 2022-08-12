0d81412d37f40651f654bc10f4cf3a7fe2818a49
Daniela Diaz Graziani <danieladiazgraziani12@gmail.com>
2022-08-12 02:55:59 -0300

    Add projects descriptions

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,21 +1,52 @@
 import React from "react";
-import recipesIdeas from '../public/assets/projects/RecipesIdeas.png'
-import freeStyle from '../public/assets/projects/freestyle.png'
-import takeASeat from '../public/assets/projects/takeaseat.png'
+import recipesIdeas from "../public/assets/projects/RecipesIdeas.png";
+import freeStyle from "../public/assets/projects/freestyle1.png";
+import takeASeat from "../public/assets/projects/takeaseat1.png";
 import ProjectsItem from "./ProjectsItem";
 
 export default function Projects() {
+
+
   return (
     <div id="projects" className="w-full">
       <div className="max-w-[1040px] mx-auto px-2 py-16">
         <h2 className="uppercase tracking-widest mb-12">Proyectos</h2>
         <div className="grid lg:grid-cols-1 md:grid-cols-2 gap-8">
-            
-       <ProjectsItem title='Recipes Ideas' backgroundImg={recipesIdeas} projectUrl='https://recipes-ideas.vercel.app/' projectRepo='https://github.com/DanielaGraziani/PI-FOODS'/>
-       
-       <ProjectsItem title='Free Style Store' backgroundImg={freeStyle} projectUrl='https://free-style-store.vercel.app/' projectRepo='https://github.com/DanielaGraziani/front-free-style'/>
-       
-       <ProjectsItem title='Take a Seat' backgroundImg={takeASeat} projectUrl='https://takeaseat.vercel.app/' projectRepo='https://github.com/DanielaGraziani/Take-A-Seat'/>
+          <ProjectsItem
+            title="Recipes Ideas"
+            backgroundImg={recipesIdeas}
+            projectUrl="https://recipes-ideas.vercel.app/"
+            projectRepo="https://github.com/DanielaGraziani/PI-FOODS"
+            description='Desarrollo de una SPA (Single Page Application).
+            Todos los componentes fueron desarrollados con CSS sin uso de librerías externas.
+            La aplicación consume datos desde la API "Spoonacular".
+            Algunos de los features agregados fueron: búsquedas, ordenamiento, filtrado y 
+            formulario controlado.'
+            technologies="React | Redux | Node | Express | Sequelize - PostgreSQL | Vercel"
+          />
+
+          <ProjectsItem
+            title="Free Style Store"
+            backgroundImg={freeStyle}
+            projectUrl="https://free-style-store.vercel.app/"
+            projectRepo="https://github.com/DanielaGraziani/front-free-style"
+            description="Creación de una E-commerce orientada a la venta de indumentaria y accesorios.
+            Algunos de los features son: login, búsqueda, filtrado, formulario controlado, 
+            carrito de compra, pasarela de pago, notificaciones, favoritos."
+            technologies="React | Redux | Tailwind CSS | Vercel | Node | Express | Heroku | Sequelize - PostgreSQL | Firebase"
+          />
+
+          <ProjectsItem
+            title="Take a Seat"
+            backgroundImg={takeASeat}
+            projectUrl="https://takeaseat.vercel.app/"
+            projectRepo="https://github.com/DanielaGraziani/Take-A-Seat"
+            description='Desarrollo de una SPA (Single Page Application) implementando React.
+            Todos los componentes fueron desarrollados con CSS sin uso de librerías
+            externas.
+            La aplicación consume datos desde la API "The Movie Database".'
+            technologies="React"
+          />
         </div>
       </div>
     </div>
--- components/ProjectsItem.jsx
+++ components/ProjectsItem.jsx
@@ -6,29 +6,32 @@ export default function ProjectsItem({
   backgroundImg,
   projectUrl,
   projectRepo,
+  description, 
+  technologies
 }) {
   return (
-    <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to [#709dff]">
+    <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]">
       <Image
         className="rounded-xl group-hover:opacity-10"
         src={backgroundImg}
         alt="/"
       />
       <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
-        <h3 className="text-2xl text-white tracking-wider text-center">
+        <h3 className="text-2xl md:text-3xl text-white tracking-wider text-center font-serif">
           {title}
         </h3>
-        <p className="pb-4 text-white text-center">React JS</p>
 
-       <div className="flex space-x-20">
+        <p className="mt-6 font-medium text-white">Tecnologías: {technologies}</p>
+      
+       <p className="hidden md:block mt-4 md:mt-9 text-justify font-medium text-white">Descripción: {description}</p>  
+
+       <div className="flex ml-4 md:mt-6 md:space-x-20 md:items-center md:justify-center">
        <a href={projectUrl} target="_blank" rel="noreferrer">
-          {/* <p className="text-center p-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer">
-            Demo
-          </p> */}
-          <button className="px-6 py-2 mt-4 mr-8">Demo</button>
+          <button className="px-6 py-2 mt-4 mr-8 cursor-pointer">Demo</button>
         </a>
+
         <a href={projectRepo} target="_blank" rel="noreferrer">
-        <button className="px-8 py-2 mt-4 mr-8">Code</button>
+        <button className="px-8 py-2 mt-4 mr-8 cursor-pointer">Code</button>
         </a>
        </div>
 
