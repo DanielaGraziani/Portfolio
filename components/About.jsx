f13bc22591a94995668e0a74ce19e6180daa9c07
Daniela Diaz Graziani <danieladiazgraziani12@gmail.com>
2022-08-12 02:55:18 -0300

    change view mobile

--- components/About.jsx
+++ components/About.jsx
@@ -8,18 +8,12 @@ export default function About() {
       <div className="max-w-[1040px] m-auto md:grid grid-cols-3 gap-8">
         <div className="col-span-2 text-justify">
           <h2 className="uppercase tracking-widest">Sobre mi</h2>
-          <p className="py-4">
+          <p className="py-4 px-4 md:px-0">
           Hace un par de años me topé con un curso de HTML y CSS el cual sin imaginármelo, me abriría las puertas a un mundo increíble. Lo que comenzó como una simple curiosidad se convirtió en amor por la programación.
           </p>
-          <p className="py-4">
+          <p className="py-4 px-4 md:px-0">
           Cautivada por lo intrigante, desafiante y divertido que puede ser la  programación me sentí rápidamente atraída por aprender más. Desde ese  entonces paso mi tiempo construyendo sitios web interactivos incorporando distintas tecnologías y librerías que dan rienda suelta a mi imaginación.
           </p>
-          {/* <p className="py-4">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam
-            vero harum sunt minima pariatur dolore culpa suscipit cumque
-            dignissimos nesciunt, enim expedita corrupti. Beatae, quibusdam
-            excepturi? Magnam nemo at error.
-          </p> */}
         </div>
           <AboutLottie/>
       </div>
