82add20b0c5fe3ccd750c9412ff71bf96ed225c5
Daniela Diaz Graziani <danieladiazgraziani12@gmail.com>
2022-08-12 00:03:44 -0300

    add external link to demo

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -10,10 +10,12 @@ export default function Projects() {
       <div className="max-w-[1040px] mx-auto px-2 py-16">
         <h2 className="uppercase tracking-widest mb-12">Proyectos</h2>
         <div className="grid lg:grid-cols-1 md:grid-cols-2 gap-8">
-
-       <ProjectsItem title='Recipes Ideas' backgroundImg={recipesIdeas} projectUrl='/' projectRepo='https://github.com/DanielaGraziani/PI-FOODS'/>
-       <ProjectsItem title='Free Style Store' backgroundImg={freeStyle} projectUrl='/' projectRepo='https://github.com/DanielaGraziani/front-free-style'/>
-       <ProjectsItem title='Take a Seat' backgroundImg={takeASeat} projectUrl='/' projectRepo='https://github.com/DanielaGraziani/Take-A-Seat'/>
+            
+       <ProjectsItem title='Recipes Ideas' backgroundImg={recipesIdeas} projectUrl='https://recipes-ideas.vercel.app/' projectRepo='https://github.com/DanielaGraziani/PI-FOODS'/>
+       
+       <ProjectsItem title='Free Style Store' backgroundImg={freeStyle} projectUrl='https://free-style-store.vercel.app/' projectRepo='https://github.com/DanielaGraziani/front-free-style'/>
+       
+       <ProjectsItem title='Take a Seat' backgroundImg={takeASeat} projectUrl='https://takeaseat.vercel.app/' projectRepo='https://github.com/DanielaGraziani/Take-A-Seat'/>
         </div>
       </div>
     </div>
