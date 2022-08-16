556dbdf4f27b13afbc537e0ba6aea40df94cdd52
Daniela Diaz Graziani <danieladiazgraziani12@gmail.com>
2022-08-15 21:27:22 -0300

    change project styles

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import recipesIdeas from "../public/assets/projects/RecipesIdeas.png";
 import freeStyle from "../public/assets/projects/freestyle1.png";
 import takeASeat from "../public/assets/projects/takeaseat1.png";
+import weatherApp from '../public/assets/projects/weather1.png'
 import ProjectsItem from "./ProjectsItem";
 
 export default function Projects() {
@@ -9,19 +10,16 @@ export default function Projects() {
 
   return (
     <div id="projects" className="w-full">
-      <div className="max-w-[1040px] mx-auto px-2 py-16">
+      <div className="max-w-[1240px] mx-auto px-2 py-16">
         <h2 className="uppercase tracking-widest mb-12">Proyectos</h2>
-        <div className="grid lg:grid-cols-1 md:grid-cols-2 gap-8">
+        <div className="grid lg:grid-cols-2 md:grid-cols-2 gap-8">
           <ProjectsItem
             title="Recipes Ideas"
             backgroundImg={recipesIdeas}
             projectUrl="https://recipes-ideas.vercel.app/"
             projectRepo="https://github.com/DanielaGraziani/PI-FOODS"
-            description='Desarrollo de una SPA (Single Page Application).
-            Todos los componentes fueron desarrollados con CSS sin uso de librerías externas.
-            La aplicación consume datos desde la API "Spoonacular".
-            Algunos de los features agregados fueron: búsquedas, ordenamiento, filtrado y 
-            formulario controlado.'
+            description='
+            La aplicación consume datos desde la API "Spoonacular".'
             technologies="React | Redux | Node | Express | Sequelize - PostgreSQL | Vercel"
           />
 
@@ -30,10 +28,8 @@ export default function Projects() {
             backgroundImg={freeStyle}
             projectUrl="https://free-style-store.vercel.app/"
             projectRepo="https://github.com/DanielaGraziani/front-free-style"
-            description="Creación de una E-commerce orientada a la venta de indumentaria y accesorios.
-            Algunos de los features son: login, búsqueda, filtrado, formulario controlado, 
-            carrito de compra, pasarela de pago, notificaciones, favoritos."
-            technologies="React | Redux | Tailwind CSS | Vercel | Node | Express | Heroku | Sequelize - PostgreSQL | Firebase"
+            description="Creación de una E-commerce orientada a la venta de indumentaria y accesorios"
+            technologies="React | Redux | Tailwind | Node | Express | Sequelize - PostgreSQL | Firebase"
           />
 
           <ProjectsItem
@@ -41,10 +37,18 @@ export default function Projects() {
             backgroundImg={takeASeat}
             projectUrl="https://takeaseat.vercel.app/"
             projectRepo="https://github.com/DanielaGraziani/Take-A-Seat"
-            description='Desarrollo de una SPA (Single Page Application) implementando React.
-            Todos los componentes fueron desarrollados con CSS sin uso de librerías
-            externas.
+            description='
             La aplicación consume datos desde la API "The Movie Database".'
+            technologies="React"
+          />
+
+            <ProjectsItem
+            title="Weather App"
+            backgroundImg={weatherApp}
+            projectUrl="https://weather-app-dd.vercel.app/"
+            projectRepo="https://github.com/DanielaGraziani/Weather-App"
+            description='
+            La aplicación consume datos desde la API "Open Weather".'
             technologies="React"
           />
         </div>
--- components/ProjectsItem.jsx
+++ components/ProjectsItem.jsx
@@ -16,18 +16,18 @@ export default function ProjectsItem({
         src={backgroundImg}
         alt="/"
       />
-      <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
-        <h3 className="text-2xl md:text-3xl text-white tracking-wider text-center font-serif">
+      <div className="md:mt-4 hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+        <h3 className="text-2xl text-white tracking-wider text-center font-serif">
           {title}
         </h3>
 
-        <p className="mt-3 md:mt-6 md:font-medium text-white">Tecnologías: {technologies}</p>
+        <p className="mt-2 md:mt-6 md:font-medium text-white">Tecnologías: {technologies}</p>
       
-       <p className="hidden md:block mt-4 md:mt-9 text-justify font-medium text-white">Descripción: {description}</p>  
+       <p className="hidden md:block mt-4 md:mt-3 text-justify font-medium text-white">Descripción: {description}</p>  
 
-       <div className="flex ml-4 md:mt-6 md:space-x-20 md:items-center md:justify-center">
+       <div className="flex ml-4 md:mt-3 md:space-x-20 md:items-center md:justify-center">
        <a href={projectUrl} target="_blank" rel="noreferrer">
-          <button className="px-6 py-2 mt-4 mr-8 cursor-pointer">Demo</button>
+          <button className="px-6 py-2 mt-4 mr-8 cursor-pointer">Project</button>
         </a>
 
         <a href={projectRepo} target="_blank" rel="noreferrer">
